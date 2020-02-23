834ee2e16cbcaa69cf60c6c79d94e72b0dc28d1d
Nicola Musatti <nicola.musatti@gmail.com>
2020-02-23 19:19:59 +0100

    Added Sphinx virtualenv installation

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -53,3 +53,9 @@ def test_mercurial(host):
     res = host.run('/opt/*venv*/hg/bin/hg --version')
 
     assert res.rc == 0
+
+
+def test_sphinx(host):
+    res = host.run('/opt/*venv*/*/bin/sphinx-quickstart --version')
+
+    assert res.rc == 0
