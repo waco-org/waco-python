7577bec9b3a8845ac70337e878019bfa517d489f
Nicola Musatti <nicola.musatti@gmail.com>
2021-12-26 00:22:34 +0100

    Correct tests and test environments

--- molecule/supported/tests/test_default.py
+++ molecule/supported/tests/test_default.py
@@ -16,11 +16,11 @@ def test_hosts_file(host):
 
 
 def test_python3(host):
-    host.check_output('/usr/bin/python3 --version').find('3') > -1
+    assert '3' in host.check_output('/usr/bin/python3 --version')
 
 
 def test_python2(host):
-    host.check_output('/usr/bin/python2 --version').find('2') > -1
+    assert '2' in host.check_output('/usr/bin/python2 --version 2>&1')
 
 
 def test_source_python(host):
@@ -29,8 +29,8 @@ def test_source_python(host):
     assert f.user == 'python'
     assert f.group == 'python'
 
-    host.check_output('/opt/Python-3.8/bin/python3.8 --version').find(
-            '3.8.6') > -1
+    assert '3.8.12' in host.check_output(
+        '/opt/Python-3.8/bin/python3.8 --version')
 
 
 def test_venv_python(host):
@@ -39,8 +39,8 @@ def test_venv_python(host):
     assert f.user == 'python'
     assert f.group == 'python'
 
-    host.check_output('/opt/venv-3.8/dev/bin/python3.8 --version').find(
-            '3.8.6') > -1
+    assert '3.8.12' in host.check_output(
+        '/opt/venv-3.8/dev/bin/python3.8 --version')
 
 
 def test_ansible(host):
